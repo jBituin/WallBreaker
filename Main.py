fba55bd82218d35c8b4d7d4442bc493715533263
LaughNot <bituinjoemel@gmail.com>
2018-09-25 22:35:20 +0800

    Finalized

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 #Initialization
 import pygame, random
-from Wheel import *
 
 pygame.init()
 pygame.mixer.init()
@@ -24,26 +23,291 @@ text = pygame.font.SysFont("Century Gothic", 50)
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("2D")
 
-ibil = Mastermind()
 
-scoreText = Label(screen.get_width() - 75, 13, 30, (255, 255, 255))
-wallHP = Label(screen.get_width() / 2, screen.get_height() / 2 - 50, 30, (255, 255, 255))
+def LoadImages(path, count):
+    images = []
+    for i in range(count):
+        image = pygame.image.load(path % (str(i))).convert()
+        image = pygame.transform.scale(image, (25, 25))
+        images.append(image)
+    return images
 
-# Wall creations
-wallLeft = Wall(screen.get_height() - 50, 25, 0, 25)
-wallTop = Wall(25, screen.get_width() - 50, 25, 0)
-wallRight = Wall(screen.get_height() - 50, 25, screen.get_width() - 25, 25)
-wallBottom = Wall(25, screen.get_width() - 50, 25, screen.get_height() - 25)
+direction = [1, -1]
+class Mastermind(pygame.sprite.Sprite):
 
-# background = pygame.Surface(screen.get_size())
-background = pygame.image.load('Resources/bg.png')
-background = pygame.transform.scale(background, screen.get_size())
-background = background.convert()
+    def __init__(self):
+        super().__init__()
 
-clock = pygame.time.Clock()
+        self.screen = screen
+        self.images = LoadImages("Resources/portal (%s).png", 3)
+        self.images = [pygame.transform.scale(image, (100, 100)) for image in self.images]
+        self.index = 0
 
-score = 0
-bestScore = 0
+        self.image = self.images[self.index]
+
+        '''self.image = pygame.Surface((75, 75))
+        self.image = self.image.convert()
+        self.image.fill((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))'''
+
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = self.screen.get_width() / 2
+        self.rect.centery = self.screen.get_height() / 2
+
+        self.currentFrame = 0
+        self.animationFrame = 6
+
+    def update(self):
+        self.updateColor()
+
+    def updateColor(self):
+        '''self.currentFrame += 1
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 0
+            self.image.fill((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))'''
+
+        self.currentFrame += 1
+
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+
+
+# Class for mobs
+class Mobs(Mastermind):
+    choice = [-5, -4, -3, 3, 4, 5]
+
+    def __init__(self):
+        super().__init__()
+        self.screen = screen
+        self.images = LoadImages("Resources/FireT_%s.png", 6)#Pass directory of images
+        self.images = [pygame.transform.scale(image, (50, 50)) for image in self.images]
+        self.index = 0
+        self.image = self.images[self.index]
+
+        self.rect = self.image.get_rect()
+
+        # Spawn coordinates -- Around Mastermind
+        self.rect.centerx = random.randrange(self.screen.get_width() / 2 - 75, self.screen.get_width() / 2 + 75)
+        self.rect.centery = random.randrange(self.screen.get_height() / 2 - 75, self.screen.get_height() / 2 + 75)
+
+        self.width = self.rect.width
+        self.height = self.rect.height
+
+        # Random speed
+        self.dx = random.choice(self.choice)
+        self.dy = random.choice(self.choice)
+
+        # Number of images in the directory
+        self.animationFrame = 6
+        self.currentFrame = 0
+
+    def update(self):
+        self.rect.centerx += self.dx
+        self.rect.centery += self.dy
+
+        self.updateFrame()
+
+    # Prevents the mob from passing through the screen
+    # Also the method that damages the wall
+    def reverse(self, direction):
+        if direction == "X":
+            self.dx = self.dx * -1
+
+        if direction == "Y":
+            self.dy = self.dy * -1
+
+    # Changes image every 6 tick
+    def updateFrame(self):
+        self.currentFrame += 1
+
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+
+
+class Aids(Mastermind):
+    choice = [-7, -6, -4, 4, 6, 7]
+
+    def __init__(self):
+        super().__init__()
+
+        self.screen = screen
+        self.images = LoadImages("Resources/iceball (%s).png", 8)  # Pass directory of images
+        self.images = [pygame.transform.scale(image, (50, 50)) for image in self.images]
+        self.index = 0
+        self.image = self.images[self.index]
+
+        self.rect = self.image.get_rect()
+
+        # Spawn coordinates -- Around Mastermind
+        self.rect.centerx = random.randrange(self.screen.get_width() / 2 - 75, self.screen.get_width() / 2 + 75)
+        self.rect.centery = random.randrange(self.screen.get_height() / 2 - 75, self.screen.get_height() / 2 + 75)
+
+        self.width = self.rect.width
+        self.height = self.rect.height
+
+        # Random speed
+        self.dx = random.choice(self.choice)
+        self.dy = random.choice(self.choice)
+
+        # Number of images in the directory
+        self.animationFrame = 6
+        self.currentFrame = 0
+
+    def update(self):
+        self.rect.centerx += self.dx
+        self.rect.centery += self.dy
+
+        self.updateFrame()
+    def reverse(self, direction):
+        if direction == "X":
+            self.dx = self.dx * -1
+
+        if direction == "Y":
+            self.dy = self.dy * -1
+
+    def updateFrame(self):
+        self.currentFrame += 1
+
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+
+
+class Healer(pygame.sprite.Sprite):
+    choice = [-1, 1]
+
+    def __init__(self):
+        super().__init__()
+        self.screen = screen
+        self.images = LoadImages("Resources/leaf%s.png", 4)#Pass directory of images
+        self.images = [pygame.transform.scale(image, (25, 25)) for image in self.images]
+        self.images = [image.convert() for image in self.images]
+        self.index = 0
+        self.image = self.images[self.index]
+
+        self.rect = self.image.get_rect()
+
+        # Spawn coordinates -- Around Mastermind
+        self.rect.centerx = random.randrange(self.screen.get_width() / 2 - 75, self.screen.get_width() / 2 + 75)
+        self.rect.centery = random.randrange(self.screen.get_height() / 2 - 75, self.screen.get_height() / 2 + 75)
+
+        self.width = self.rect.width
+        self.height = self.rect.height
+
+        # Random speed
+        self.dx = random.choice(self.choice)
+        self.dy = random.choice(self.choice)
+
+        # Number of images in the directory
+        self.animationFrame = 24
+        self.currentFrame = 0
+
+    def update(self):
+        self.rect.centerx += self.dx
+        self.rect.centery += self.dy
+
+        #self.reverse()
+        self.updateFrame()
+
+    # Prevents the mob from passing through the screen
+    def reverse(self, direction):
+        if direction == "X":
+            self.dx = self.dx * -1
+
+        if direction == "Y":
+            self.dy = self.dy * -1
+
+    def updateFrame(self):
+        self.currentFrame += 1
+
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 10
+            self.animationFrame = 16
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+            self.image = self.image.convert()
+
+
+class Label(pygame.sprite.Sprite):
+
+    def __init__(self, xPos, yPos, size, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.SysFont("Arial", size)
+        self.text = ""
+        self.center = (xPos, yPos)
+        self.color = color
+
+    def update(self):
+        self.image = self.font.render(self.text, 1, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.center
+
+
+class Wall(pygame.sprite.Sprite):
+    HP = 125
+    def __init__(self, height, width, xPos, yPos):
+        super().__init__()
+        self.screen = screen
+        self.width = width
+        self.height = height
+        self.imageGreen = pygame.image.load("Resources/Wall.jpg")
+        self.imageFire = pygame.image.load("Resources/wallFire.png")
+
+        self.imageGreen = pygame.transform.scale(self.imageGreen, (width, height))
+        self.imageFire = pygame.transform.scale(self.imageFire, (width, height))
+        self.xPos = xPos
+        self.yPos = yPos
+
+        #pygame.draw.rect(self.image, (255, 0, 255), (xPos, yPos, width, height))
+        self.rect = pygame.Rect(xPos, yPos, width, height)
+
+    def update(self):
+        if self.HP < 50:
+            self.image = self.imageFire
+
+        else:
+            self.image = self.imageGreen
+
+
+class Display(pygame.sprite.Sprite):
+
+    def __init__(self, path, count, xPos, yPos):
+        super().__init__()
+        self.screen = screen
+        self.images = LoadImages(path, count)#Pass directory of images
+        self.images = [pygame.transform.scale(image, (50, 50)) for image in self.images]
+        self.index = 0
+        self.image = self.images[self.index]
+
+        self.rect = self.image.get_rect()
+
+        # Spawn coordinates -- Around Mastermind
+        self.rect.centerx = xPos
+        self.rect.centery = yPos
+
+        self.width = self.rect.width
+        self.height = self.rect.height
+
+        # Number of images in the directory
+        self.animationFrame = count + 1
+        self.currentFrame = 0
+
+    def update(self):
+        self.updateFrame()
+
+    # Changes image every 6 tick
+    def updateFrame(self):
+        self.currentFrame += 1
+
+        if self.currentFrame >= self.animationFrame:
+            self.currentFrame = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
 
 def GetBestScore():
     # Default high score
@@ -199,11 +463,15 @@ def MenuScreen():
     wall = pygame.font.SysFont("Lato", 100)
     Breaker = pygame.font.SysFont("Lato", 100)
 
-    sampleGroup.add(fire, ice, heal, fireText, iceText, healText)
 
     playText = text.render("Play", 1, (0, 0, 0))
     instructionText = text.render("Click objects to destroy", 1, (0, 0, 0))
     bestText = text.render("Best: " + str(GetBestScore()), 1, (0, 0, 0))
+
+    copyrightText = Label(530, 470, 14, (255, 255, 255))
+    copyrightText.text = "(c) Copyright Disclaimer. Credits to all images" 
+
+    sampleGroup.add(fire, ice, heal, fireText, iceText, healText, copyrightText)
 
     while not play:
         wallText = wall.render("Wall", 1, (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
@@ -234,6 +502,26 @@ def MenuScreen():
         clock.tick(60)
         pygame.display.flip()
 
+ibil = Mastermind()
+
+scoreText = Label(screen.get_width() - 75, 13, 30, (255, 255, 255))
+wallHP = Label(screen.get_width() / 2, screen.get_height() / 2 - 50, 30, (255, 255, 255))
+
+# Wall creations
+wallLeft = Wall(screen.get_height() - 50, 25, 0, 25)
+wallTop = Wall(25, screen.get_width() - 50, 25, 0)
+wallRight = Wall(screen.get_height() - 50, 25, screen.get_width() - 25, 25)
+wallBottom = Wall(25, screen.get_width() - 50, 25, screen.get_height() - 25)
+
+# background = pygame.Surface(screen.get_size())
+background = pygame.image.load('Resources/bg.png')
+background = pygame.transform.scale(background, screen.get_size())
+background = background.convert()
+
+clock = pygame.time.Clock()
+
+score = 0
+bestScore = 0
 
 def Main():
     # The one in the center
